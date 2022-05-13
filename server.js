a7acac0b89d3d9d185aec4481cba0224d18f09b0
Mandee <102609655+MandeeRiz@users.noreply.github.com>
2022-05-13 17:53:36 -0400

    tip calculator

--- server.js
+++ server.js
@@ -2,8 +2,17 @@ const express= require ("express")
 const app = express();
 
 
-app.get("/greeting/:name", function(req,res){
-    res.send('"hello ' + req.params.name + "!")
+//app.get("/greeting/:name", function(req,res){
+//    res.send('"hello ' + req.params.name + "!")
+//})
+
+app.get("/tip/:total/:tipPercentage", function(req,res){
+    let total = req.params.total; 
+    let tipPerc = req.params.tipPercentage;
+    let fraction = tipPerc / total;
+    fraction *= 100;
+    let sendThis = fraction.toString();
+    res.send(sendThis);
 })
 
 
